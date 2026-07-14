f8a9d5f8aac27934fdd4fd76c68bb1e3cf5c8d61
ThiagoLFE <thiago.rib1905@gmail.com>
2026-07-14 19:25:38 -0300

    feat: post thumbnail files to the video

--- handler_upload_thumbnail.go
+++ handler_upload_thumbnail.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
+	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/database"
 	"github.com/google/uuid"
 )
 
@@ -28,10 +30,64 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 		return
 	}
 
-
 	fmt.Println("uploading thumbnail for video", videoID, "by user", userID)
 
 	// TODO: implement the upload here
+	const maxMemory = 10 << 20
+	r.ParseMultipartForm(maxMemory)
 
-	respondWithJSON(w, http.StatusOK, struct{}{})
+	file, fileHeader, err := r.FormFile("thumbnail")
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Unable to parse form file", err)
+		return
+	}
+
+	thumbnailContentType := fileHeader.Header.Get("Content-Type")
+
+	thumbnailData, err := io.ReadAll(file)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Fail to read the thumbnail data", err)
+		return
+	}
+
+	video, err := cfg.db.GetVideo(videoID)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Fail to get the video", err)
+		return
+	}
+
+	if video.UserID != userID {
+		respondWithError(w, http.StatusUnauthorized, "You can only update your own videos", err)
+		return
+	}
+
+	thumbnail := thumbnail{
+		data:      thumbnailData,
+		mediaType: thumbnailContentType,
+	}
+
+	videoThumbnails[videoID] = thumbnail
+
+	thumbnailURL := fmt.Sprintf("http://localhost:%s/api/thumbnails/%s", cfg.port, videoID)
+
+	video.ThumbnailURL = &thumbnailURL
+	if err := cfg.db.UpdateVideo(video); err != nil {
+		respondWithError(w, http.StatusInternalServerError, "fail to update video", err)
+		return
+	}
+
+	updatedVideo, err := cfg.db.GetVideo(videoID)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Fail to get the video", err)
+		return
+	}
+
+	respondWithJSON(w, http.StatusOK, database.Video{
+		ID:                updatedVideo.ID,
+		CreatedAt:         updatedVideo.CreatedAt,
+		UpdatedAt:         updatedVideo.UpdatedAt,
+		ThumbnailURL:      updatedVideo.ThumbnailURL,
+		VideoURL:          updatedVideo.VideoURL,
+		CreateVideoParams: updatedVideo.CreateVideoParams,
+	})
 }
